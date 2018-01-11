04e285037ef7732ed3cb39bfc5e42b761ea25826
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2018-01-11 11:03:06 +0200

    tested object programming

---
+++ object-method/TaskPerson/TaskPerson/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPerson
+{
+    public class Person
+    {
+        //Fields
+        public string Name;
+        private int age;
+
+        //Constructor
+        public Person()
+        {
+            Name = "unknown";
+            age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            this.age = age;
+        }
+
+        //Methods
+        public void GrowOld()
+        {
+            this.age++;
+        }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"Nimi: {Name} \nIkä {this.age}\nAikuinen: {IsAdult()}");
+        }
+
+        public bool IsAdult()
+        {
+            //"return this.age >= 18;" Tämä on lyhyempi ja kauniimpi tapa tehdä asiat
+
+            if (this.age >= 18)
+                return true;
+            else
+                return false;
+        }
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+    }
+}
---
+++ object-method/TaskPerson/TaskPerson/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TaskPerson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person personMikko = new Person();
+            personMikko.Name = "Mikko K";
+            personMikko.GrowOld();
+            personMikko.PrintPersonInfo();
+
+            Person newPerson = new Person("Johnson MacDickfield", 89);
+            newPerson.PrintPersonInfo();
+            newPerson.GrowOld();
+            newPerson.PrintPersonInfo();
+            Console.ReadKey();
+        }
+    }
+}
