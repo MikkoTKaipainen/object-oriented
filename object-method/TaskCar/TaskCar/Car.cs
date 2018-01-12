0f7b884785bfc26580f4cd79b567bd739f00fb88
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2018-01-12 10:59:48 +0200

    added new task

--- object-method/TaskCar/TaskCar/Car.cs
+++ object-method/TaskCar/TaskCar/Car.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace TaskCar
 {
+    //Tehty yhteistyössä Timon kanssa
     class Car
     {
+        //Fields
+        public string Brand;
+        public int Speed;
+
+        //Methods
+        public void AskData()
+        {
+            Console.Write("Car Brand: ");
+            Brand = Console.ReadLine();
+            Console.Write("Car Speed: ");
+            Speed = int.Parse(Console.ReadLine());
+        }
+
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Your car is: {Brand} \nYour speed is: {Speed}");
+        }
+
+        public void ShowCarInfo2()
+        {
+
+            Console.WriteLine($"Your car is still: {Brand} \nYour accelerated speed is: {Speed}");
+        }
+
+        public void ShowCarInfo3()
+        {
+            Console.WriteLine($"Your car is yet one more time: {Brand} \nYour decreased speed is : {Speed}");
+        }
+
+        public void Accelerate(int speed)
+        {
+            if (speed >= 0)
+            Speed += speed;
+        }
+
+        public void Brake()
+        {
+            Speed = (Speed / 10) * 9;
+        }
+
     }
 }
--- object-method/TaskCar/TaskCar/Program.cs
+++ object-method/TaskCar/TaskCar/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
 namespace TaskCar
+        //Tehty yhteistyössä Timon kanssa
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car car1 = new Car();
+            Car car2 = new Car();
+            Car car3 = new Car();
+
+            car1.AskData();
+            car2.AskData();
+            car3.AskData();
+            Console.WriteLine();
+            car1.ShowCarInfo();
+            car2.ShowCarInfo();
+            car3.ShowCarInfo();
+            Console.WriteLine();
+            car1.Accelerate(100);
+            car2.Accelerate(100);
+            car3.Accelerate(100);
+            Console.WriteLine();
+            car1.ShowCarInfo2();
+            car2.ShowCarInfo2();
+            car3.ShowCarInfo2();
+            Console.WriteLine();
+            car1.Brake();
+            car2.Brake();
+            car3.Brake();
+            Console.WriteLine();
+            car1.ShowCarInfo3();
+            car2.ShowCarInfo3();
+            car3.ShowCarInfo3();
+
+            Console.ReadKey();
         }
     }
 }
