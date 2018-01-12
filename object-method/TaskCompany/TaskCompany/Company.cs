63f7b25f962d66349794f597838623d307e10e7a
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2018-01-12 16:57:39 +0200

    started new project

---
+++ object-method/TaskCompany/TaskCompany/Company.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskCompany
+{
+    class Company
+    {
+        //Muodostetaan kentät
+        public string Title;
+        public string Address;
+        public int Phone;
+        public double Outcome;
+        public double Expense;
+    }
+
+    public string()
+}
