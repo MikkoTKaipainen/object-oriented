6e64b91b4ebea42de29336ef02d3d936fe5cb355
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2018-01-12 09:38:06 +0200

    added new lines to taskBook

---
+++ object-method/TaskBook/TaskBook/Book.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskBook
+    //Tehty yhteistyönä Timon kanssa
+{
+    class Book
+    {
+        //Fields
+        //Kirjalle määritetään kirjailia, nimi, id-numero sekä kappalehinta nimiset muuttujat
+        public string Title;
+        public string Author;
+        public int Id;
+        public double Price;
+
+        //Constructor
+        //Määritellään muuttujille arvot
+        public Book()
+        {
+            Title = "Koodivelhon päiväkirjat";
+            Author = "Johnson MacDickfield";
+            Id = 170992;
+            Price = 29.99;
+        }
+
+        public Book(string title, string author, int id, double price)
+        {
+            Title = title;
+            Author = author;
+            Id = id;
+            Price = price;
+        }
+
+        //Methods
+        //Tulostaa kirjan tiedot näytölle
+        public void PrintBookInfo()
+        {
+            Console.WriteLine($"Title: {Title} \nAuthor: {Author} \nId: {Id} \nPrice: {Price}");
+            Console.WriteLine($"{CompareBook()}");
+        }
+
+        public string CompareBook(Book book)
+        {
+            if (this.Price > book.Price)
+                return ($"{this.Title} on kalliimpi kuin {book.Title} kirja");
+            else if (this.Price < book.Price)
+                return ($"{this.Title} on halvempi kuin {book.Title} kirja");
+            else
+                return ($"{this.Title} on saman hintainen kuin {book.Title} kirja");
+        }
+
+    }
+}
---
+++ object-method/TaskBook/TaskBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TaskBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            book.PrintBookInfo();
+
+            Book newBook = new Book("Koodivelhon salainen päiväuni", "Johnston MccMacemoor", 100791, 39.99);
+            newBook.PrintBookInfo();
+            Console.ReadKey();
+        }
+    }
+}
