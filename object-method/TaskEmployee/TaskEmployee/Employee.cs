a683f5a417bd14e3b3352a2632b10df6cad04dc7
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2018-01-19 11:05:13 +0200

    started new project

---
+++ object-method/TaskEmployee/TaskEmployee/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskEmployee
+{
+    class Employee
+    {
+        public string Name;
+        public int Id;
+        public string Position;
+        public int Salary;
+
+        public void EmployeeInfo()
+        {
+            Name = "Jorma";
+            Id = 101;
+            Position = "Boss";
+            Salary = 10000;
+        }
+
+        public void CompareSalary()
+        {
+            
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"{Name} \n" +
+                $"{Id} \n" +
+                $"{Position} \n" +
+                $"{Salary}");
+        }
+
+
+    }
+}
---
+++ object-method/TaskEmployee/TaskEmployee/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.PrintEmployeeInfo();
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee();
+            employees[1] = new Employee();
+            employees[2] = new Employee();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
