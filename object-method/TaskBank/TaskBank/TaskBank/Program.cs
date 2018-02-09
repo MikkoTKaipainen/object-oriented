e5ae60423d96baf07b0e23f8920a1841563bad65
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2018-02-09 11:07:53 +0200

    started new project

---
+++ object-method/TaskBank/TaskBank/TaskBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TaskBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string machineFormat = BankUtil.CorrectNumber("5227082-33351");
+            Console.WriteLine(BankUtil.CorrectNumber("5227082-33351"));
+            Console.WriteLine(BankUtil.IsValidAccount(machineFormat));
+            Console.WriteLine($"Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
