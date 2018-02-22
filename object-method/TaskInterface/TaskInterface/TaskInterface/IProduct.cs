46d58518a8cc31598b1964359d7de1ac67f62e00
MikkoTKaipainen <mikko.kaipainen@student.saimia.fi>
2018-02-22 10:52:22 +0200

    updataoirna

---
+++ object-method/TaskInterface/TaskInterface/TaskInterface/IProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskInterface
+{
+    interface IProduct
+    {
+        public string GetProduct(string name)
+        {
+        
+        }
+        
+        public int CountValue(int áprice, int amount)
+        {
+            áprice = 20;
+            amount = 10;
+            return áprice * amount;
+        }
+    }
+}
---
+++ object-method/TaskInterface/TaskInterface/TaskInterface/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskInterface
+{
+    class Product
+    {
+        private string name;
+        private int áprice;
+        private int amount;
+
+        public Product(string name, int áprice, int amount)
+        {
+            this.name = name;
+            this.áprice = áprice;
+            this.amount = amount;
+        }
+
+        public int Amount { get => amount; set => amount = value; }
+        public int Áprice { get => áprice; set => áprice = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
